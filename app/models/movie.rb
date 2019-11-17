c0ee0ef6ac7ba7349d7aea6e0353252e363dd157
Mars Le <hp.mars.le@gmail.com>
2019-11-17 23:08:51 +0000

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+  # attr_accessor :id, :title, :release_date, :director, :lead, :in_theaters
+  
+  # def initialize(id: nil, title: nil, release_date: nil, director: nil, lead: nil, in_theaters: nil)
+  #   @id = id
+  #   @title = title
+  #   @release_date = release_date
+  #   @director = director
+  #   @lead = lead
+  #   @in_theaters = in_theaters
+  # end
 end
